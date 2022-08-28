5fbedf6fb2cbaeb9263126825a5f4cb1e38bfae9
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-08-28 17:41:54 +0200

    New version for Google Play

--- mobile/src/main/java/com/thomaskuenneth/c64watchface/MainActivity.kt
+++ mobile/src/main/java/com/thomaskuenneth/c64watchface/MainActivity.kt
@@ -2,15 +2,20 @@ package com.thomaskuenneth.c64watchface
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.preference.PreferenceManager
 import com.thomaskuenneth.c64watchface.databinding.MainBinding
 import com.thomaskuenneth.common.C64
 import java.util.concurrent.Callable
-
-private const val REQUEST_CODE_PREFS = 123
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +28,14 @@ class MainActivity : AppCompatActivity() {
             binding.c64watchface.invalidate()
         shouldBeRunning
     }
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            c64.setup()
+            binding.c64watchface.postInvalidate()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +45,19 @@ class MainActivity : AppCompatActivity() {
         c64.setup()
         binding.c64watchface.tag = c64
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, windowInsets ->
+            if (windowInsets.isVisible(WindowInsetsCompat.Type.statusBars()))
+                supportActionBar?.show()
+            else
+                supportActionBar?.hide()
+            windowInsets
+        }
+        binding.root.setOnClickListener {
+            hideSystemBars()
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            hideSystemBars()
+        }, 3000L)
     }
 
     override fun onPause() {
@@ -45,14 +71,6 @@ class MainActivity : AppCompatActivity() {
         c64.pulse(callback)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_PREFS) {
-            c64.setup()
-            binding.c64watchface.postInvalidate()
-        }
-    }
-
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.options, menu)
         return super.onPrepareOptionsMenu(menu)
@@ -61,16 +79,22 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivityForResult(intent, REQUEST_CODE_PREFS)
+                launcher.launch(Intent(this, SettingsActivity::class.java))
                 true
             }
             R.id.info -> {
-                val intent = Intent(this, InfoActivity::class.java)
-                startActivity(intent)
+                startActivity(Intent(this, InfoActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun hideSystemBars() {
+        with(WindowCompat.getInsetsController(window, window.decorView)) {
+            systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
         }
     }
 }
